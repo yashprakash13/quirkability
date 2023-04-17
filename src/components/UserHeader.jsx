859b9278273ff4ca3b1a0dd3129fed0c22fdcbb5
Yash Prakash <yashprakash13@gmail.com>
2023-04-17 12:51:34 -0400

    feature: added public user header component.

---
+++ src/components/UserHeader.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+const UserHeader = ({ name, bio, pic_url }) => {
+  return (
+    <div className="border-sm border-secondary-focus shadow-sm flex flex-col md:flex-row justify-center items-center px-7 py-3 gap-7">
+      <img
+        src="https://source.unsplash.com/00ByEXKcSkA/2000x2000"
+        className="rounded-full w-32 h-32 md:w-32 md:h-32"
+      />
+      <div className="flex flex-col justify-center items-center md:items-start gap-4">
+        <div className="text-2xl font-medium font-serif">{name}</div>
+        <div className="text-lg text-center md:text-left">{bio}</div>
+      </div>
+    </div>
+  )
+}
+
+export default UserHeader
--- src/pages/publicpages/publicUser.jsx
+++ src/pages/publicpages/publicUser.jsx
@@ -1,9 +1,19 @@
 import { useParams } from "react-router-dom"
+import UserHeader from "../../components/UserHeader"
 
 const PublicUser = () => {
   const { username } = useParams()
 
-  return <div>Got user: {username}</div>
+  return (
+    <div className="container flex flex-col mx-auto p-3">
+      <UserHeader
+        name={"Danille Murphy"}
+        bio={
+          "Forem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis."
+        }
+      />
+    </div>
+  )
 }
 
 export default PublicUser
