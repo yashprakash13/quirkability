dcd50cd6b468516b5d779c394535526508f21ddd
Yash Prakash <yashprakash13@gmail.com>
2023-06-25 22:12:00 -0400

    feature: added profile pic component.

--- src/pages/dashboardpages/settings.jsx
+++ src/pages/dashboardpages/settings.jsx
@@ -1,7 +1,7 @@
 import usePayment from "../../hooks/use-payment"
 import { useAuth } from "../../context/auth"
 import { createStripeConnectAccount } from "../../services/backendCalls"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import {
   getUserDetailsFromId,
   updateUserprofileTable,
@@ -9,6 +9,7 @@ import {
 import { PencilIcon, XMarkIcon } from "@heroicons/react/24/solid"
 import { toast } from "react-toastify"
 import { ClipLoader } from "react-spinners"
+import DefaultProfilePic from "../../assets/defaultProfilePic"
 
 const Settings = () => {
   const { user } = useAuth()
@@ -19,6 +20,14 @@ const Settings = () => {
   const [bio, setBio] = useState(null)
   const [bioTextArea, setBioTextArea] = useState(null)
   const [loading, setLoading] = useState(false)
+
+  const inputRef = useRef(null)
+  const [file, setFile] = useState(null)
+  const [imagePreviewUrl, setImagePreviewUrl] = useState("")
+  const [isImageSelected, setImageSelected] = useState(false)
+  const [formErrors, setFormErrors] = useState({})
+
+  const allowedInputRegex = /^[a-zA-Z0-9!,';\s]+$/
 
   async function handleStripeConnect() {
     if (!stripeId) {
@@ -81,29 +90,107 @@ const Settings = () => {
     getBio()
   }
 
+  function handleFileChange(e) {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      const img = new Image()
+      img.src = reader.result
+      img.onload = () => {
+        if (
+          img.width > 200 &&
+          img.height > 200 &&
+          (file.type === "image/jpeg" || file.type === "image/png")
+        ) {
+          setFormErrors((prevState) => ({ ...prevState, fileError: "" }))
+          setFile(file)
+          setImagePreviewUrl(reader.result)
+          setImageSelected(true)
+        } else {
+          setFormErrors((prevState) => ({
+            ...prevState,
+            fileError:
+              "Invalid image. Please select a JPG or PNG image with both dimensions above 200 pixels.",
+          }))
+        }
+      }
+    }
+    if (file) {
+      reader.readAsDataURL(file)
+    }
+  }
+
+  function saveImage() {
+    // Placeholder function for saving the selected image
+    console.log("Image saved:", file)
+    setImageSelected(false)
+    setFormErrors({})
+    setFile(null)
+  }
+
+  function handleChangePhotoButtonClick() {
+    inputRef.current.click()
+    setFormErrors({})
+  }
+
+  function handleProfilePhotoCancelButtonClick() {
+    setImageSelected(false)
+    setFile(null)
+    setFormErrors({})
+    setImagePreviewUrl("")
+  }
+
   return (
     <div className="container mx-auto flex flex-col p-3 gap-11 mt-11">
       <div className="flex flex-col justify-start gap-7 md:gap-11">
         <div className="text-2xl font-medium">Profile Picture</div>
         <div className="flex flex-col md:flex-row justify-start md:justify-between md:items-center gap-5">
           <div className="flex flex-col md:flex-row gap-7 md:items-center">
-            <img
-              src="https://source.unsplash.com/Ud4bLEy4gC0"
-              className="rounded-full w-36 h-36"
-            />
+            {imagePreviewUrl ? (
+              <img
+                src={imagePreviewUrl}
+                alt="Selected"
+                className="w-36 h-36 rounded-full"
+              />
+            ) : (
+              <DefaultProfilePic
+                classStyles={
+                  "w-28 h-28 rounded-full border-sm border-secondary-focus"
+                }
+              />
+            )}
             <div className="text-md md:text-lg font-serif w-full md:w-3/5">
               Your photo will appear on your store and in all your products. It
-              should be at least 200px x 200px in size and can be in JPG or PNG
+              should be at least 200px x 200px in size and can be in JPEG or PNG
               formats.
             </div>
           </div>
           <div
             className="w-2/3 md:w-1/3 lg:w-1/5 text-lg md:text-xl font-bold flex justify-center items-center border-sm rounded-br-2xl px-7 py-2 text-primary-default bg-secondary-focus cursor-pointer hover:bg-primary-default hover:text-secondary-focus transition-all duration-300"
-            //
+            onClick={isImageSelected ? saveImage : handleChangePhotoButtonClick}
           >
-            Change
+            {isImageSelected ? "Save" : "Change Photo"}
           </div>
+          <div
+            className={`w-2/3 md:w-1/3 lg:w-1/5 text-lg md:text-xl font-bold flex justify-center items-center border-sm rounded-br-2xl px-7 py-2 text-primary-default bg-secondary-focus cursor-pointer hover:bg-primary-default hover:text-secondary-focus transition-all duration-300 ${
+              !isImageSelected && "hidden"
+            }`}
+            onClick={handleProfilePhotoCancelButtonClick}
+          >
+            Remove
+          </div>
+          <input
+            type="file"
+            id="fileInput"
+            accept="image/jpeg, image/png"
+            onChange={handleFileChange}
+            className="hidden"
+            ref={inputRef}
+          />
         </div>
+        {formErrors.fileError && (
+          <div className="text-xl text-alert-dark">{formErrors.fileError}</div>
+        )}
       </div>
       <div className="w-full border-t-sm border-light-highlight opacity-25"></div>
       <div className="flex flex-col gap-3">
