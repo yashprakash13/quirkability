07d99f8400e886aae49666b552b452461581ad27
Yash Prakash <yashprakash13@gmail.com>
2023-06-28 11:41:50 -0400

    feature: stored stripe id in cache.

--- src/hooks/use-payment.jsx
+++ src/hooks/use-payment.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { getStripeId } from "../services/supabaseHelpers"
+import { readFromCache, writeToCache } from "../utils"
 
 const usePayment = (user) => {
   const [stripeId, setStripeId] = useState(null)
@@ -7,8 +8,15 @@ const usePayment = (user) => {
   useEffect(() => {
     async function fetchData() {
       if (user) {
-        const stripe_connect_id = await getStripeId(user.id)
-        setStripeId(stripe_connect_id)
+        if (readFromCache("sub_account_id")) {
+          console.log("Read stripe id from cache.")
+          setStripeId(readFromCache("sub_account_id"))
+        } else {
+          console.log("Fetching stripe id from db.")
+          const stripe_connect_id = await getStripeId(user.id)
+          setStripeId(stripe_connect_id)
+          writeToCache("sub_account_id", stripe_connect_id)
+        }
       }
     }
     fetchData()
