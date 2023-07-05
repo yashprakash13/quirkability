2665dc2c820afe52bbc2102333b026cf01b6781e
Yash Prakash <yashprakash13@gmail.com>
2023-07-04 20:44:35 -0400

    feature: added sentry

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from "./App"
 import "./index.css"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import * as Sentry from "@sentry/react"
+
+Sentry.init({
+  dsn: "https://38ed4e701c844a8b994d74b738ca6ef9@o4505474010644480.ingest.sentry.io/4505474011824128",
+})
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
