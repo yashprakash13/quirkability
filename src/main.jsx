c64171dd1ede041e44354f66380a67f8a650079a
Yash Prakash <yashprakash13@gmail.com>
2023-07-10 00:07:19 -0400

    rollback: same as before

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import "./index.css"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
 import * as Sentry from "@sentry/react"
-import { HelmetProvider } from "react-helmet-async"
 
 Sentry.init({
   dsn: "https://38ed4e701c844a8b994d74b738ca6ef9@o4505474010644480.ingest.sentry.io/4505474011824128",
@@ -13,9 +12,7 @@ Sentry.init({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-      <ToastContainer />
-    </HelmetProvider>
+    <App />
+    <ToastContainer />
   </React.StrictMode>
 )
