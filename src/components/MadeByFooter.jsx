715b133a8308f2998f08102670be3d7ce5d3156d
Yash Prakash <yashprakash13@gmail.com>
2023-07-20 22:09:06 -0400

    feature: better footer

--- src/components/MadeByFooter.jsx
+++ src/components/MadeByFooter.jsx
@@ -1,18 +1,21 @@
 const MadeByFooter = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="flex gap-1">
-        <div className="text-xs font-extralight font- font-serif italic">
-          made by
+    <div className="flex gap-11 justify-center items-center">
+      <div className="flex gap-3 justify-center items-center bg-primary-default px-4 py-2 rounded-full">
+        <div className="flex gap-1">
+          <div className="text-sm font-extralight font- font-serif italic">
+            © 2023
+          </div>
         </div>
+        <a
+          className="text-md font-bold font-serif text-light-highlight hover:text-secondary-focus hover:underline"
+          href="https://twitter.com/csandyash"
+          target="_blank"
+        >
+          Yash Prakash{" "}
+        </a>
+        <div className="font-thin text-xs">(all rights reserved.)</div>
       </div>
-      <a
-        className="text-md font-bold font-serif text-secondary-focus hover:underline"
-        href="https://twitter.com/csandyash"
-        target="_blank"
-      >
-        Yash Prakash
-      </a>
     </div>
   )
 }
